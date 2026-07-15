be023f98e170e5e63b49ea1ada0c012a1b38aa90
Tomoooji <168693891+Tomoooji@users.noreply.github.com>
2026-07-15 10:27:20 +0900

    done?

--- example/closterium_ps4/closterium_ps4.ino
+++ example/closterium_ps4/closterium_ps4.ino
@@ -1,2 +1,18 @@
-#include <ESP32_Controller.h>
+#define CONTROLLER_TYPE CONTROLLER_PS4
+#include "ESP32_Controller.h"
 
+struct WheelCommand{
+  int speed_R;
+  int speed_L;
+  void apply(){
+    this->speed_L = PS4.LStickY();
+    this->speed_R = PS4.RStickY();
+  }
+} input;
+
+Config_PS4 config = {"00:00:00:00:00:00"};
+
+Controller<WheelCommand> ps4(&config,&input);
+
+void setup(){}
+void loop(){}
